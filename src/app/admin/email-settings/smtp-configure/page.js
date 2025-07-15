0aa6f1587b566e98e7df3e1e397b89573ba6e3cb
rohitwebstep <rohitwebstep@gmail.com>
2025-07-15 17:12:41 +0530

    update - 11

---
+++ src/app/admin/email-settings/smtp-configure/page.js
@@ -0,0 +1,8 @@
+import SmtpForm from '@/components/admin/email-settings/SmtpForm'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div><SmtpForm /></div>
+    )
+}
